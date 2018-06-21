7c4ef80330b45458af5e7d51fdd7dfc3ad2bbfd6
Anthony Scatchell <scatchell@gmail.com>
2018-06-21 15:47:28 +0100

    Added serial port connection to fast flash and changed to point directly at mos binary

--- setupDeviceWithTemplate.sh
+++ setupDeviceWithTemplate.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+SERIAL_PORT=$(ls /dev/cu* | grep -v Bluetooth | fzf --height=3 -1 -0 || { echo >&2 "${WHITE}No devices detected.${NC}"; exit 1; })
+
 echo "Building...."
-(mos build ./ --platform esp8266)
+(~/.mos/bin/mos build ./ --platform esp8266 --port "$SERIAL_PORT")
 echo "Flashing build zip..."
-(mos flash build/fw.zip)
+(~/.mos/bin/mos flash build/fw.zip --port "$SERIAL_PORT")
 echo "Setting up wifi..."
-mos wifi twguest "<enter tw guest password here>"
+~/.mos/bin/mos wifi twguest "<enter tw guest password here>" --port "$SERIAL_PORT"
 echo "Setting up aws iot..."
-mos aws-iot-setup --aws-region eu-west-2 --aws-iot-policy mos-default
+~/.mos/bin/mos aws-iot-setup --aws-region eu-west-2 --aws-iot-policy mos-default --port "$SERIAL_PORT"
